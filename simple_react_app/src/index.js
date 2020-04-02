ae456ad5fb50a9ea76237c02b245d1d97b923baa
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-02 19:55:23 +0300

    added style and media queries

--- simple_react_app/src/index.js
+++ simple_react_app/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './index.css'
 
 const Person = ({img,name,job,children}) => {
   const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`
   return (
-    <article>
+    <article className="person">
       <img src={url} 
       alt="person"/>
       <h4>{name}</h4>
       <h4>{job}</h4>
+      {children}
     </article>
   )
 }
@@ -16,9 +18,11 @@ const Person = ({img,name,job,children}) => {
 
 const PersonList = () => {
   return (
-    <section>
+    <section className="person-list">
       <Person img="29" name= "Dan" job="developer" />
-      <Person img="30" name="Tony" job="designer" />
+      <Person img="30" name="Tony" job="designer">
+        <p>this is some additional info</p>
+        </Person>
       <Person img="25" name="Mike" job="waiter" />
     </section>
   )
